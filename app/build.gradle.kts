d201f4fb19bf9cc7e1a68cec2babc43a3829683a
Nanda Arya Putra <nandaarya404@gmail.com>
2023-11-27 21:03:27 +0700

    Add location detection and UI Main Layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,10 +58,11 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.11.0")
-
 
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
--- app/src/main/java/com/example/tara/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/tara/ui/main/MainActivity.kt
@@ -1,18 +1,84 @@
 package com.example.tara.ui.main
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
+import com.example.tara.R
 import com.example.tara.ViewModelFactory
 import com.example.tara.databinding.ActivityMainBinding
 import com.example.tara.ui.login.LoginActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var mainViewModel: MainViewModel
 
+    private var fusedLocationProviderClient: FusedLocationProviderClient? = null
+    private var longitude: Double = 0.0
+    private var latitude: Double = 0.0
+    private var city: String = ""
+    private val interval: Long = 10000 // 10seconds
+    private val fastestInterval: Long = 5000 // 5 seconds
+    private lateinit var mLastLocation: Location
+    private lateinit var mLocationRequest: LocationRequest
+    private val requestPermissionCode = 999
+
+    private val requestNotificationPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            requestBackgroundLocationPermissionLauncher.launch(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+            Toast.makeText(this, "Notifications permission granted", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Notifications permission rejected", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val requestBackgroundLocationPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {                Toast.makeText(this, "Background permission granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Background Location Permission Denied", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            if (permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true && permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true) {
+                // All permissions are granted, proceed with location updates.
+                requestNotificationPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+
+                startLocationUpdates()
+            } else {
+                Toast.makeText(this, "Izinkan Aplikasi Mengakses Lokasi", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -28,8 +94,146 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        binding.btnLogout.setOnClickListener {
-            mainViewModel.logout()
+        setOptionMenu()
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        mLocationRequest = LocationRequest.create()
+
+        checkGPSIsEnabled()
+
+        checkForPermission(this)
+        startLocationUpdates()
+
+//        binding.btnLogout.setOnClickListener {
+//            mainViewModel.logout()
+//        }
+    }
+
+    private fun setOptionMenu() {
+        binding.topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_option -> {
+//                    val intent = Intent(this, SettingActivity::class.java)
+//                    startActivity(intent)
+                    true
+                }
+                R.id.menu_logout -> {
+                    mainViewModel.logout()
+                    true
+                }
+                else -> false
+            }
         }
+    }
+
+    private fun startLocationUpdates() {
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = interval
+        mLocationRequest.fastestInterval = fastestInterval
+
+        val builder = LocationSettingsRequest.Builder()
+        builder.addLocationRequest(mLocationRequest)
+        val locationSettingsRequest = builder.build()
+        val settingsClient = LocationServices.getSettingsClient(this)
+        settingsClient.checkLocationSettings(locationSettingsRequest)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        fusedLocationProviderClient!!.requestLocationUpdates(
+            mLocationRequest, mLocationCallback, Looper.myLooper()!!
+        )
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            locationResult.lastLocation
+            Log.d("MainActivity", "callback: $latitude $longitude")
+            locationResult.lastLocation?.let { locationChanged(it) }
+            latitude = locationResult.lastLocation?.latitude ?: 0.0
+            longitude = locationResult.lastLocation?.longitude ?: 0.0
+            city = getCityName(latitude, longitude)
+//            binding.longitudeText.text = "Longitude: $longitude"
+//            binding.latitudeText.text = "Latitude: $latitude"
+            binding.tvCity.text = "City: $city"
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun checkForPermission(context: Context) {
+        val permissions = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+        )
+
+        if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && context.checkSelfPermission(
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            // Permissions are already granted, proceed with location updates.
+            startLocationUpdates()
+        } else {
+            // Request permissions.
+            requestPermissionLauncher.launch(permissions)
+        }
+    }
+
+    private fun checkGPSIsEnabled() {
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            val builder = AlertDialog.Builder(this)
+            builder.setMessage("The location permission is disabled. Do you want to enable it?")
+                .setCancelable(true)
+                .setPositiveButton("Yes") { _, _ ->
+                    startActivityForResult(
+                        Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS), 10
+                    )
+                }.setNegativeButton("No") { dialog, _ ->
+                    dialog.cancel()
+                }
+            val alert: AlertDialog = builder.create()
+            alert.show()
+        }
+    }
+
+    fun locationChanged(location: Location) {
+        mLastLocation = location
+        longitude = mLastLocation.longitude
+        latitude = mLastLocation.latitude
+        city = getCityName(latitude, longitude)
+//        binding.longitudeText.text = "Longitude: $longitude"
+//        binding.latitudeText.text = "Latitude: $latitude"
+        binding.tvCity.text = city
+        Log.d("MainActivity", "function: $latitude $longitude, City: ")
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == requestPermissionCode) {
+            if (grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                // All permissions are granted, proceed with location updates.
+                startLocationUpdates()
+            } else {
+                Toast.makeText(this, "Permission Denied 2", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun getCityName(lat: Double,long: Double): String {
+        val cityName: String?
+        val geoCoder = Geocoder(this, Locale.getDefault())
+        val address = geoCoder.getFromLocation(lat,long,3)
+
+        cityName = address?.get(0)?.adminArea ?: "Kota Tidak Terdeteksi"
+        return cityName
     }
 }
